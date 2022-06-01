2f61c95910ea0af24b2b7b4f52fac4a2d27d00c4
diana.martyn <diana.martyn@ihsmarkit.com>
2022-06-01 11:28:55 +0300

    unused import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,20 +7,19 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { RequestManagerComponent } from './components/request-manager/request-manager.component';
 import { AddRequestComponent } from './components/add-request/add-request.component';
-import { SpinnerComponent } from './components/spinner/spinner.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
-import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
-import { provideAuth,getAuth } from '@angular/fire/auth';
-import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 import { AngularFireModule } from '@angular/fire/compat';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
 import { VerifyEmailComponent } from './components/verify-email/verify-email.component';
-import { EditRequestComponent} from "./components/edit-request/edit-request.component";
-import { ViewRequestComponent} from "./components/view-request/view-request.component";
+import { EditRequestComponent } from './components/edit-request/edit-request.component';
+import { ViewRequestComponent } from './components/view-request/view-request.component';
 import { RequestComponent } from './components/request/request.component';
 import { NgSelectModule } from '@ng-select/ng-select';
 import { SearchFilterPipe } from './pipes/search-filter.pipe';
@@ -33,7 +32,6 @@ import { SearchFilterPipe } from './pipes/search-filter.pipe';
     AddRequestComponent,
     EditRequestComponent,
     ViewRequestComponent,
-    SpinnerComponent,
     PageNotFoundComponent,
     LoginComponent,
     RegisterComponent,
@@ -52,9 +50,9 @@ import { SearchFilterPipe } from './pipes/search-filter.pipe';
     AngularFireModule.initializeApp(environment.firebase),
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth()),
-    provideFirestore(() => getFirestore())
+    provideFirestore(() => getFirestore()),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
