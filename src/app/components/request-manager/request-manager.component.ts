240c1e7acd6dba90993fc6dfff45649c034ca1b0
diana.martyn <diana.martyn@ihsmarkit.com>
2022-06-02 23:48:09 +0300

    refactor code

--- src/app/components/request-manager/request-manager.component.ts
+++ src/app/components/request-manager/request-manager.component.ts
@@ -1,40 +1,43 @@
 import { Component, OnInit } from '@angular/core';
-import {AuthService} from "../../services/auth.service";
-import {DataService} from "../../services/data.service";
-import {IRequest} from "../../models/IRequest";
+import { AuthService } from '../../services/auth.service';
+import { DataService } from '../../services/data.service';
+import { IRequest } from '../../models/IRequest';
 
 @Component({
   selector: 'app-request-manager',
   templateUrl: './request-manager.component.html',
-  styleUrls: ['./request-manager.component.css']
+  styleUrls: ['./request-manager.component.css'],
 })
 export class RequestManagerComponent implements OnInit {
   filteredString: string = '';
   requestsList: IRequest[] = [];
 
-  constructor(private auth: AuthService, private dataService: DataService) { }
+  constructor(private auth: AuthService, private dataService: DataService) {}
 
   ngOnInit(): void {
-    this.getAllRequests()
+    this.getAllRequests();
   }
 
-  getAllRequests(){
-    this.dataService.getAllRequests().subscribe(res => {
-
-      this.requestsList = res.map( (e: any) => {
-        const data = e.payload.doc.data();
-        data.id =e.payload.doc.id;
-        return data;
-      })
-
-    },() => {
-      alert('Error while fetching data')
-    })
+  getAllRequests() {
+    this.dataService.getAllRequests().subscribe(
+      (res) => {
+        this.requestsList = res.map((e: any) => {
+          const data = e.payload.doc.data();
+          data.id = e.payload.doc.id;
+          return data;
+        });
+      },
+      () => {
+        alert('Error while fetching data');
+      }
+    );
+  }
+  emptyString() {
+    this.filteredString = '';
   }
 
-  deleteRequest(request:IRequest){
-    if(window.confirm("Do you really want to delete this request?"))
-    this.dataService.deleteRequest(request)
+  deleteRequest(request: IRequest) {
+    if (window.confirm('Do you really want to delete this request?'))
+      this.dataService.deleteRequest(request);
   }
-
 }
