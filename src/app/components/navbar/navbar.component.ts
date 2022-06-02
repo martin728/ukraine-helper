5413a3beb3ab89b4513be12ad7b047d0a3b59968
diana.martyn <diana.martyn@ihsmarkit.com>
2022-06-03 02:13:34 +0300

    hide logout btn in navbar, fix sign in with google method

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Component({
   selector: 'app-navbar',
@@ -7,7 +8,7 @@ import { AuthService } from '../../services/auth.service';
   styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent {
-  constructor(private auth: AuthService) {}
+  constructor(private auth: AuthService, public afAuth: AngularFireAuth) {}
 
   register() {
     this.auth.logout();
